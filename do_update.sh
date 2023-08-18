f13ac603a9358f49c29a55d261d839175f0e530c
Robert Nürnberg <robert.nurnberg@gmx.de>
2023-08-18 16:08:55 +0200

    logplot (#5)
    * logplot
    
    * cutoff 200

--- do_update.sh
+++ do_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 plotdata.py
+python3 plotdata.py --logplot --cutOff 200
 
 git add caissatrack.png caissatrackpv.png
 git diff --staged --quiet || git commit -m "update plots"
