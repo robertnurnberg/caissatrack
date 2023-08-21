3ae915a4ae1070593751d2739d324d8a80ba4688
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-08-21 12:09:10 +0200

    refactor plotting script

--- plotdata.py
+++ plotdata.py
@@ -27,63 +27,96 @@ class matedata:
                     self.depths.append(ast.literal_eval(dict2Str))
 
     def create_graph(self, cutOff=125, logplot=False, pv=False, negplot=False):
-        dateOld, dateNew = str(self.date[0]), str(self.date[-1])
-        if pv:
-            dictOld, dictNew = self.depths[0], self.depths[-1]
-            # negative PV lengths mean PVs that end in a terminal draw
-            for d in [dictOld, dictNew]:
-                for key, value in list(d.items()):
+        color, edgecolor, label = ["red", "blue"], ["yellow", "black"], [None, None]
+        dictList = [None, None]  # list for the two dicts to plot
+        rangeMin, rangeMax = None, None
+        for Idx in [0, -1]:
+            if pv:
+                dictList[Idx] = self.depths[Idx].copy()
+                # negative PV lengths mean PVs that end in a terminal draw
+                for key, value in list(dictList[Idx].items()):
                     if key <= -10000 or (key < 0 and not negplot):
                         newkey = -(-key % 10000) if key <= -10000 else key
                         if not negplot:
                             newkey = -newkey
                         if key != newkey:
-                            d[newkey] = d.get(newkey, 0) + value
-                            del d[key]
-            rangeOld = min(dictOld.keys()), max(dictOld.keys())
-            rangeNew = min(dictNew.keys()), max(dictNew.keys())
-        else:
-            dictOld, dictNew = self.evals[0], self.evals[-1]
-            rangeOld = min(dictOld.keys()), max(dictOld.keys())
-            rangeNew = min(dictNew.keys()), max(dictNew.keys())
-            for d in [dictOld, dictNew]:
+                            dictList[Idx][newkey] = dictList[Idx].get(newkey, 0) + value
+                            del dictList[Idx][key]
+                mi, ma = min(dictList[Idx].keys()), max(dictList[Idx].keys())
+                if negplot and mi < 0 and ma > 0:
+                    negmax = max([k for k in dictList[Idx].keys() if k < 0])
+                    posmin = min([k for k in dictList[Idx].keys() if k > 0])
+                    cup = r"$\cup$"
+                    label[Idx] = (
+                        str(self.date[Idx])
+                        + f"   (in [{mi}, {negmax}]{cup}[{posmin}, {ma}])"
+                    )
+                else:
+                    label[Idx] = str(self.date[Idx]) + f"   (in [{mi}, {ma}])"
+            else:
+                dictList[Idx] = self.evals[Idx]
+                mi, ma = min(dictList[Idx].keys()), max(dictList[Idx].keys())
+                label[Idx] = str(self.date[Idx]) + f"   (in [{mi}, {ma}])"
                 for key in [-cutOff, cutOff]:
-                    if key not in d:
-                        d[key] = 0
-                for key, value in list(d.items()):
+                    if key not in dictList[Idx]:
+                        dictList[Idx][key] = 0
+                for key, value in list(dictList[Idx].items()):
                     if key < -cutOff:
-                        d[-cutOff] += value
-                        del d[key]
+                        dictList[Idx][-cutOff] += value
+                        del dictList[Idx][key]
                     elif key > cutOff:
-                        d[cutOff] += value
-                        del d[key]
-
-        rangeMin = min(list(dictOld.keys()) + list(dictNew.keys()))
-        rangeMax = max(list(dictOld.keys()) + list(dictNew.keys()))
+                        dictList[Idx][cutOff] += value
+                        del dictList[Idx][key]
+            mi, ma = min(dictList[Idx].keys()), max(dictList[Idx].keys())
+            rangeMin = mi if rangeMin is None else min(mi, rangeMin)
+            rangeMax = ma if rangeMax is None else max(ma, rangeMax)
         fig, ax = plt.subplots()
-        listNew = [key for key, val in dictNew.items() for _ in range(val)]
         perBin = 2 if pv else 1 if cutOff <= 100 else 3  # values per bin
-        ax.hist(
-            listNew,
-            range=(rangeMin, rangeMax),
-            bins=(rangeMax - rangeMin) // perBin,
-            density=True,
-            alpha=0.5,
-            color="blue",
-            edgecolor="black",
-            label=dateNew + f"   (in [{rangeNew[0]}, {rangeNew[1]}])",
-        )
-        listOld = [key for key, val in dictOld.items() for _ in range(val)]
-        ax.hist(
-            listOld,
-            range=(rangeMin, rangeMax),
-            bins=(rangeMax - rangeMin) // perBin,
-            density=True,
-            alpha=0.5,
-            color="red",
-            edgecolor="yellow",
-            label=dateOld + f"   (in [{rangeOld[0]}, {rangeOld[1]}])",
-        )
+        for Idx in [-1, 0]:
+            listIdx = [key for key, val in dictList[Idx].items() for _ in range(val)]
+            ax.hist(
+                listIdx,
+                range=(rangeMin, rangeMax),
+                bins=(rangeMax - rangeMin) // perBin,
+                density=True,
+                alpha=0.5,
+                color=color[Idx],
+                edgecolor=edgecolor[Idx],
+                label=label[Idx],
+            )
+            if pv:
+                terminal = []
+                term = [
+                    "2fold",
+                    "50 mr",  # <= -10000
+                    "stalemate",  # <= -20000
+                    "mate",  # <= -30000
+                    "TB draw",  # <= -40000
+                    "TB win",  # <= -50000
+                    "TB unknown",  # <= -60000
+                ]
+                for step in range(7):
+                    c = sum(
+                        [
+                            v
+                            for k, v in self.depths[Idx].items()
+                            if -(step + 1) * 10000 < k
+                            if k <= -step * 10000
+                        ]
+                    )
+                    if c:
+                        terminal.append(f"{term[step]} ({c})")
+                terminalStr = "Terminal PV leafs: " + ", ".join(terminal)
+                ax.text(
+                    0.1,
+                    0.02 - Idx * 0.02,
+                    terminalStr,
+                    transform=fig.transFigure,
+                    color=color[Idx],
+                    fontsize=6,
+                    family="monospace",
+                    weight="bold",
+                )
         ax.legend(fontsize=8)
         fig.suptitle(
             f"Distribution of cdb {'depths' if pv else 'evals'} in {self.prefix}.csv."
@@ -91,7 +124,7 @@ class matedata:
         if pv:
             if negplot:
                 ax.set_title(
-                    "(A negative depth means that the PV ends in a theoretical draw.)",
+                    "(A negative depth means that the PV ends in a theoretically known result, with 2folds counting as 3folds.)",
                     fontsize=6,
                     family="monospace",
                 )
