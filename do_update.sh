f008ed5f8ede6ac0f24f9c14b1a5cf13f448cf6a
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-08-25 11:05:49 +0200

    provide time series of progress indicators

--- do_update.sh
+++ do_update.sh
@@ -2,6 +2,6 @@
 
 python3 plotdata.py --logplot --cutOff 200 --negplot --logplot
 
-git add caissatrack.png caissatrackpv.png
+git add caissatrack.png caissatrackpv.png caissatracktime.png 
 git diff --staged --quiet || git commit -m "update plots"
 git push origin main >& push.log
