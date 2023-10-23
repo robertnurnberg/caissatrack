167481304e1053a8c1b7c7706ecfc5ab61753615
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-10-23 18:18:21 +0200

    40 -> 24

--- do_collect.sh
+++ do_collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ../cdblib/cdbbulkpv.py -c 40 --stable --user rob caissa_sorted_100000.epd >caissa_sorted_100000_cdbpv.epd
+python3 ../cdblib/cdbbulkpv.py -c 24 --stable --user rob caissa_sorted_100000.epd >caissa_sorted_100000_cdbpv.epd
 python3 caissatrack.py >>caissatrack.csv
 python3 extract_fens.py --shortest 1000 --ignore2folds >caissa_daily_shortest.epd
 python3 extract_fens.py --evalMin 85 --evalMax 105 >caissa_daily_edgy.epd
