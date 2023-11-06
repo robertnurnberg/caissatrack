7bfe73c2bd44e6ed2a9584cb8f1d42cf62525ddf
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-11-06 08:22:35 +0100

    widen edgy region to [80,110]

--- do_update.sh
+++ do_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 plotdata.py --logplot --cutOff 200 --negplot --logplot --edgeMin 85 --edgeMax 105
+python3 plotdata.py --logplot --cutOff 200 --negplot --logplot --edgeMin 80 --edgeMax 110
 
 git add caissatrack.png caissatrackpv.png caissatracktime.png
 git diff --staged --quiet || git commit -m "update plots"
